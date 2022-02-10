eccb9bb6f5aa1f6317e1dc01639619b1b86b5bd5
Michael Bach Kristensen <michael@seluxit.com>
2022-02-10 12:27:31 +0100

    Test - Added a network creation test.

--- test/wappstoiot_test.py
+++ test/wappstoiot_test.py
@@ -1,18 +1,16 @@
 #! /usr/bin/env python3
 
+import uuid
 import shutil
 import datetime
 from pathlib import Path
-# from pytest import mock
 
 import pytest
-import uuid
 
 from utils.generators import client_certifi_gen
-from utils.package_smithing import get_success_pkg
-import wappstoiot
+from utils import package_smithing as smithing
 
-import rich
+import wappstoiot
 
 
 class TestConnection:
@@ -34,9 +32,18 @@ class TestConnection:
                 file.write(data)
 
     @pytest.fixture
-    def mock_socket(self, mocker):
+    def mock_ssl_socket(self, mocker):
         socket = mocker.patch(
             target='wappstoiot.connections.sslsocket.ssl.SSLContext.wrap_socket',
+            autospec=True
+        )
+
+        return socket
+
+    @pytest.fixture
+    def mock_rw_socket(self, mocker):
+        socket = mocker.patch(
+            target='wappstoiot.connections.sslsocket.socket.socket',
             autospec=True
         )
 
@@ -53,6 +60,16 @@ class TestConnection:
         shutil.rmtree(self.temp, ignore_errors=True)
         self.temp.mkdir(exist_ok=True, parents=True)
 
+    @classmethod
+    def cleanup_class(self):
+        """
+        Sets up the class.
+
+        Sets locations to be used in test.
+
+        """
+        shutil.rmtree(self.temp, ignore_errors=True)
+
     @pytest.mark.parametrize(
         "url, port",
         [
@@ -62,11 +79,10 @@ class TestConnection:
             ('staging.wappsto.com', 54005),
         ]
     )
-    def test_connection(self, mock_socket, url, port):
+    def test_connection(self, mock_rw_socket, mock_ssl_socket, url: str, port: int):
         self.generate_certificates(name=url, network_uuid=uuid.uuid4())
-        # rich.print(dir(mock_socket))
-        mock_socket.return_value.recv.return_value = get_success_pkg(
-            id="lkjghtrfty",
+        mock_ssl_socket.return_value.recv.return_value = smithing.get_success_pkg(
+            pkg_id="lkjghtrfty",
             timestamp=datetime.datetime.utcnow(),
         )
         try:
@@ -79,4 +95,30 @@ class TestConnection:
         if url == 'wappsto.com':
             url = f"collector.{url}"
 
-        mock_socket.return_value.connect.assert_called_with((f"{url}", port))
+        mock_ssl_socket.return_value.connect.assert_called_with((f"{url}", port))
+
+    def test_network_creation(self, mock_rw_socket, mock_ssl_socket):
+        network_uuid = uuid.uuid4()
+        url = "wappsto.com"
+        self.generate_certificates(name=url, network_uuid=network_uuid)
+
+        cycle = smithing.get_network_create_cycle(
+            network_uuid=network_uuid,
+            changed=True
+        )
+
+        smithing.socket_generator(
+            mock_rw_socket=mock_rw_socket,
+            mock_ssl_socket=mock_ssl_socket,
+            cycle_list=cycle
+        )
+
+        try:
+            wappstoiot.config(
+                config_folder=Path(self.temp),
+            )
+            network = wappstoiot.createNetwork()
+        finally:
+            wappstoiot.close()
+
+        smithing.fail_check()
