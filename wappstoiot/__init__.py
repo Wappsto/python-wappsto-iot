547fb206804cc663cd24084ae415b53724e61501
Michael Bach Kristensen <michael@seluxit.com>
2022-03-08 14:34:55 +0100

    Bump version to 0.6.2

--- wappstoiot/__init__.py
+++ wappstoiot/__init__.py
@@ -51,7 +51,7 @@ from .utils import name_check
 #                             __init__ Setup Stuff
 # #############################################################################
 
-__version__ = "v0.6.1"
+__version__ = "v0.6.2"
 __auther__ = "Seluxit A/S"
 
 __all__ = [
