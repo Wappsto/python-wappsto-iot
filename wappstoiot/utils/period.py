9f3f226e41075f22121583061b6ac562bc894aad
Michael Bach Kristensen <michael@seluxit.com>
2023-10-06 13:46:02 +0200

    Period - Fix Type hinting errors.

--- wappstoiot/utils/period.py
+++ wappstoiot/utils/period.py
@@ -14,28 +14,49 @@ from typing import Tuple
 
 
 class PeriodClass(ABC):
+    """
+    The Abstract base class for Period.
+
+    Period is used to define a minimum report interval.
+    """
+
     period: datetime.timedelta
     call_function: Callable[[], None]
 
     @abstractmethod
-    def __init__(self, period: datetime.timedelta): ...
-
-    @abstractmethod
-    def time_to_next_period(self) -> float: ...
-
-    @abstractmethod
-    def start(
+    def __init__(
         self,
+        period: datetime.timedelta,
         function: Callable[..., Any],
-        args: Optional[Tuple[Any]]=None,
-        kwargs: Optional[Dict[str, Any]]=None,
-    ): ...
+        args: Optional[Tuple[Any]] = None,
+        kwargs: Optional[Dict[str, Any]] = None,
+    ):
+        """Initialize the period."""
+        ...
 
     @abstractmethod
-    def close(self): ...
+    def time_to_next_period(self) -> float:
+        """Return the seconds to next time period are triggered."""
+        ...
+
+    @abstractmethod
+    def start(self) -> None:
+        """Start the period logic."""
+        ...
+
+    @abstractmethod
+    def close(self) -> None:
+        """Stop the period logic."""
+        ...
 
 
 class Period(PeriodClass):
+    """
+    Used to define a minimum report interval.
+
+    The Period are always relative to UTC 00:00.
+    Period is disabled if set to: 0, None or float('inf')
+    """
     period: datetime.timedelta
     call_function: Callable[[], None]
     current_timer: threading.Timer
@@ -44,34 +65,25 @@ class Period(PeriodClass):
         self,
         period: datetime.timedelta,
         function: Callable[..., Any],
-        args: Optional[Tuple[Any]]=None,
-        kwargs: Optional[Dict[str, Any]]=None,
+        args: Optional[Tuple[Any, ...]] = None,
+        kwargs: Optional[Dict[str, Any]] = None,
     ):
         """Initialize the period."""
         self.period = period
 
-        f_args: tuple = args if args is not None else tuple()
-        f_kwargs: tuple = kwargs if kwargs is not None else {}
+        f_args: Tuple[Any, ...] = args if args is not None else tuple()
+        f_kwargs: Dict[str, Any] = kwargs if kwargs is not None else {}
         self.call_function = lambda: function(*f_args, **f_kwargs)
-
-    def __unix_day_start(self) -> float:
-        return datetime.datetime.utcnow().replace(
-            hour=0,
-            minute=0,
-            second=0,
-            microsecond=0,
-            tzinfo=datetime.UTC,
-        ).timestamp()
 
     def time_to_next_period(self) -> float:
         """Return the seconds to next time period are triggered."""
         # NOTE: datetime do not support leap seconds so unix time is just as good.
         return (- time.time()) % self.period.seconds  # next_period
 
-    def start(self):
+    def start(self) -> None:
         """Start the period logic."""
 
-        def repeat_logic():
+        def repeat_logic() -> None:
             self.call_function()
             self.current_timer = threading.Timer(
                 interval=self.time_to_next_period(),
@@ -85,6 +97,6 @@ class Period(PeriodClass):
         )
         self.current_timer.start()
 
-    def close(self):
+    def close(self) -> None:
         """Stop the period logic."""
         self.current_timer.cancel()
