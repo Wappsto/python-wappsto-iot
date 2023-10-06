3a5a9b18650022b8f5e5dfbfaaf3a10572b4ba12
Michael Bach Kristensen <michael@seluxit.com>
2023-10-06 13:46:48 +0200

    Period - Fix Type hinting + changed step to be int.

--- wappstoiot/utils/jitter.py
+++ wappstoiot/utils/jitter.py
@@ -2,12 +2,22 @@
 import threading
 import random
 
+from typing import Any
+from typing import Callable
 
 jitter_range_sec: int = 10
 
 
-def exec_with_jitter(obj, *args, **kwargs):
+def exec_with_jitter(
+    obj: Callable[..., Any],
+    *args: Any,
+    **kwargs: Any,
+) -> None:
     """Execute given function between new and the jitter range."""
-    jitter_time = random.randrange(start=0, stop=jitter_range_sec, step=0.1)
+    jitter_time = random.randrange(
+        start=0,
+        stop=jitter_range_sec * 10,
+        step=1,
+    ) / 10
     temp = threading.Timer(jitter_time, obj, args=args, kwargs=kwargs)
     temp.start()
